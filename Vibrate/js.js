075bdb251f92d83b4f4362a10bf7d208ba143a4e
G2bb <g2bb@aol.com>
2022-03-31 08:15:27 -0500

    known good vibrate

--- Vibrate/js.js
+++ Vibrate/js.js
@@ -1,5 +1,5 @@
 function vibrate(){
-    time = document.getElementsByName('seconds').value
-    window.navigator.vibrate(1000);
-    alert("bzzzz");
+    //time = document.getElementsByName('seconds').value
+    window.navigator.vibrate([500, 100, 500]);
+    //alert("bzzzz");
 }
